02ca7be733a60085eec7ec685ef395ddc2ccda19
Alec Troemel <ajtroem@pioneer2.site>
2014-11-26 17:30:06 -0600

    beginning of header files

---
+++ Course.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
---
+++ Student.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+class Student {
+	public:
+		int studentId;
+		vector<Course> courses;
+		
+	
+	private:
+}
+
+#endif
