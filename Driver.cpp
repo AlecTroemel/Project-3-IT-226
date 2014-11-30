e5a012c393fe9ddfdcef95e9604605c400a10fc6
Alec <alectroemel@hotmail.com>
2014-11-30 10:35:27 -0600

    driver update

--- Driver.cpp
+++ Driver.cpp
@@ -28,15 +28,23 @@ void addData() {
 	string firstline = "";
 	ifstream myfile(fileName.c_str()); //file stream
 
-
+	
 	if(myfile.good()) {	
 		int rows;
 		getline(myfile, firstline, '\n');	
 		cout << firstline<< "\n";
-	
-		myfile.close();
+		
+		
+		// fill queue with lines 
+		queue<string> lines;
+		string line = "";
+		while(getline(myfile,line,'\n')) {
+			lines.push(line);
+			cout << "\n" << line << "\n";		
+		}
+		
 
-		//read the first line, pushing an int corosponding to the type of column into a queue
+		//read the first line, pushing column title into a queue
 		queue<string> columnName;
 		int i = 0;	
 		while(!firstline.empty()) {
@@ -49,15 +57,33 @@ void addData() {
 				columnName.push(firstline.substr(0,temp));			
 				firstline = firstline.substr(temp+1);
 			}
-
 			i++;
 		}
-
+		
+		//go through a line and process it 
+		while(!lines.empty()) {
+			line = lines.front();
+			lines.pop();
+					
+			int size = columnName.size();
+			for(int i = 0; i <= size; i++) {
+				
+				//push front column to the back of the queue
+				string columnTitle = columnName.front();
+				columnName.pop();	
+				columnName.push(columnTitle);
+				
+				//process the item according to its column				
+			}	
+		}
+			
+		/*
+		//print out the items in the columnName Queue
 		while(!columnName.empty()) {
 			string temp = columnName.front();
 			cout  <<  temp << "\n";	
 			columnName.pop();
-		}
+		}*/
 	}
 	else {
 		cout << "file does not exist\n";
@@ -67,33 +93,19 @@ void addData() {
 void saveDataForStudent() {
 	cout << "this has not been implemented yet\n";
 }
-/*
-void menu() {
-	char choice;
-	cout << "A: Add data \nS: Save data for a student \nE: Exit\n";
-	cin >> choice;
-
-	if(choice == 'A' || choice == 'a') 
-		addData();
-	else if(choice == 'S' || choice == 's') 
-		saveDataForStudent();
-	else if(choice == 'E' || choice == 'e')
-		std::exit(0);
-	else { 
-		cout << "Please enter a valid choice\n";
-		menu(); //loop back to menu
-	}
-}
-*/
 
 int main(int argc, char *argv[]) {
-	char choice;
+
 	while(true) {
+		cin.clear();
+		char choice;
 		cout << "\nA: Add data \nS: Save data for a student \nE: Exit\n";
 		cin >> choice;
 
-		if(choice == 'A' || choice == 'a') 
+		if(choice == 'A' || choice == 'a') { 
 			addData();
+			cin.clear();
+		}
 		else if(choice == 'S' || choice == 's') 
 			saveDataForStudent();
 		else if(choice == 'E' || choice == 'e')
